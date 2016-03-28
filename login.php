50dff7102d891a8cb769b52a07210c7f5b80d0ce
Prayag Patel <ppatel96@hawk.iit.edu>
2016-03-28 10:44:46 -0500

    login page

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Random Login Form</title>
+   <link rel="stylesheet" type="text/css" href="login.css">
+    
+  </head>
+
+  <body>
+
+    <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>IIT<span>iRL</span></div>
+		</div>
+		<br>
+		<div class="login">
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+				<input type="button" value="Login">
+		</div>    
+    
+    
+  </body>
+</html>
