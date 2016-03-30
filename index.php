93c3861038985768e402c254323c01481fa31e16
Seth Carpenter <scarpen3@otsads.iit.edu>
2016-03-30 18:57:45 -0500

    Resdeisgned div layout and got rid of extraneous crap that was there from a template

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-<html >
+<html>
   <head>
     <meta charset="UTF-8">
     <title>iRL Login</title>
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-    <script src="https://use.typekit.net/kjz8pbx.js"></script>
-    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <link rel="stylesheet" type="text/css" href="css/screen.css">
   </head>
 
-  <body>
+  <body class="citybackground">
 
-    <div class="body"></div>
-		<div class="grad"></div>
-		<div class="header">
-			<div><span>iRL</span></div>
-		</div>
-		<br>
+    
+		
 		<div class="login">
-				<input type="text" placeholder="username" name="user"><br>
-				<input type="password" placeholder="password" name="password"><br>
-				<a href="main.php"><input type="button" value="Login"></a>
+			<div class="header">
+				<p>Illinois Tech</p>
+				iRL
+				
+			</div>
+	
+			<input type="text" placeholder="username" name="user"><br>
+			<input type="password" placeholder="password" name="password"><br>
+			<a href="main.php"><input type="button" value="Login"></a>
 		</div>    
     
-    
+  
   </body>
 </html>
