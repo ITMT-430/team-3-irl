d9014ba559d9ac8e5c08d96a46cf6ad0d00cc5bd
Seth Carpenter <Seth Carpenter>
2016-04-13 11:44:21 -0500

    Added seperate nav bar php to include on other pages

---
+++ nav.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/screen.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+</head>
+<nav>
+	<ul>
+		<li><a href="main.php">iRL</a></li>
+		<li><a href="settings.php">Settings</a></li>
+		<li><a href="logout.php">Logout <?php echo phpCAS::getUser(); ?></a></li>
+	</ul>
+</nav>
+<html>
