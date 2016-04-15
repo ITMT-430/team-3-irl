c1cc9ecce5d3d2e0ee1b3f3f9d1d0338f0db7bd4
Prayag Patel <ppatel96@hawk.iit.edu>
2016-04-15 18:05:10 -0500

    edit logout page, take 2

--- logout.php
+++ logout.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html>
+
 <?php//LOGOUT OF ALL SESSIONS::
 
 if(isset($_REQUEST['logout'])) {
@@ -6,3 +9,9 @@ if(isset($_REQUEST['logout'])) {
 echo "Thanks and have a good day!!";
 
 ?>
+
+<body>
+	<h1>Thank you and Have a wonderful Day!!</h1>
+</body>
+
+</html>
