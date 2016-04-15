3de58d266041fe6d9c6eedbd6df65c6cc38fbc7c
Prayag Patel <ppatel96@hawk.iit.edu>
2016-04-15 17:38:14 -0500

    Added signup page

---
+++ signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>iRL Login</title>
+    <link rel="stylesheet" type="text/css" href="css/screen.css">
+  </head>
+  <body class="citybackground">
+  <p>****Please Sign up****</p>
+  <p>****Use Hawk Information****</p>
+
+  <div class="login">
+			<div class="header">
+				<p class="iit">Illinois Tech</p>
+				iRL
+			</div>
+		<form action="settings.php" method="post">
+		    <label for="phone">Phone</label>
+		    <input id="phone" type="text" name="phone" placeholder="123-456-7899"> 
+		    <label for="facebook">Facebook</label>
+				<input id="facebook" type="text" name="facebook" placeholder="https://facebook.com/superdude"> 
+				<label for="twitter">Twitter</label>
+				<input id="twitter" type="text" name="twitter" placeholder="https://twitter.com/superdude">
+				<label for="email">Email</label>
+				<input id="email" type="text" name="email" placeholder="you@something.com">
+	
+				<input type="submit" value="submit" name="submitbutton">
+		</form>
+		</div> 
+		
+
+  </body>
