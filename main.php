554a9e9e16ca311656395fdc44f959981cd1f48f
thegeekkid <brian@geekkidconsulting.com>
2016-04-20 10:46:56 -0500

    Added a validatetoken function.

--- main.php
+++ main.php
@@ -11,6 +11,25 @@
     //Please store $servertoken in the database token field.
     //Please store $experation in the database experation field.
   }
+  
+  //Use the function below to see if user is already authenticated.  Will return true if they are or false if they aren't.
+  function validatetoken(); {
+    //Please read the experation and servertoken fields into the vars below.
+    $experation = ""
+    $servertoken = ""
+    
+    //Don't touch below.
+    $authed = false;
+    if (!($experation < time())) {
+      if(isset($_COOKIE["rltoken"])) {
+        $clienttoken = $_COOKIE["rltoken"]
+        if ($clienttoken == hash('ripemd160', $servertoken) {
+          $authed = true;
+        }
+      }
+    }
+    return authed;
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
