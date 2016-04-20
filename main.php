beff6f67503d4ff5eb1d11127a1d7e18aa531eeb
thegeekkid <brian@geekkidconsulting.com>
2016-04-20 10:38:16 -0500

    Added a generatetoken function.

--- main.php
+++ main.php
@@ -1,3 +1,17 @@
+<?php
+  $servertoken = "";
+  $clienttoken = "";
+  //The function below should be run once we have authenticated the user via CAS.
+  function generatetoken(); {
+    $raw = "";
+    $servertoken = hash('sha256', $raw);
+    $clienttoken = hash('ripemd160', $servertoken);
+    $experation = (time()+3600); //expire in 1 hour
+    setcookie("rltoken", $clienttoken, $experation);
+    //Please store $servertoken in the database token field.
+    //Please store $experation in the database experation field.
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
