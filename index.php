25ee02537354e7395111ec670c7ab85887cc5ab4
Seth Carpenter <seth.g.carpenter@gmail.com>
2016-04-23 20:19:11 -0500

    Added meta tatg for mobile responsiveness

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>iRL Login</title>
     <link rel="stylesheet" type="text/css" href="css/screen.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, scale-to-fit=no">
   </head>
 
   <body class="citybackground">
