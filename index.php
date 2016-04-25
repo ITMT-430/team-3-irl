e5afa66e3d4fda9a9386250d572adcc22e4c3ff6
Prayag Patel <ppatel96@hawk.iit.edu>
2016-04-25 11:12:03 -0500

    Index available number

--- index.php
+++ index.php
@@ -13,13 +13,35 @@
 		
 		<div class="login">
 			<div class="header">
-				<p class="iit">Illinois Tech</p>
+				<p class="iit">Welcome to Illinois Tech</p>
 				iRL
 			</div>
 			
 			<a href="main.php"><input type="button" value="Login with Hawk Credentials"></a>
 		</div>    
     <a href="http://gph.is/26m6Sdh" target="_blank"><img src="images/googleplus.png" alt="" width="10" height="13" /></a>
+
+   <?php
+   include 'connect.php';
+
+   //Display Number of users available
+   $sql = "SELECT * FROM user_table WHERE available > " .(time()/60);
+   $result = $mysqli->query($sql);
+
+    if ($result->num_rows > 0){
+    	//output the data
+    	while($row = $result->fetch_assoc()){
+    		echo"There are $row available";
+    	}
+    }
+
+  else{
+  	echo "No one is Available";
+  }
+
+  $mysqli->close();
+	
+   ?>
   
   </body>
 </html>
