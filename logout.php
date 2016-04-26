6b3bc6710cbdfb4a11c6f47e7d51fff0b3bf398f
Seth Carpenter <seth.g.carpenter@gmail.com>
2016-04-25 23:27:14 -0500

    ADded styled logout page

--- logout.php
+++ logout.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="UTF-8">
+    <title>iRL Login</title>
+    <link rel="stylesheet" type="text/css" href="css/screen.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, scale-to-fit=no">
+  </head>
+
+  <body class="citybackground">
+		
+		<div class="login">
+			<div class="header">
+    <img src="images/logo.png" />
+ 	 </div>
+			
+			<h1>See you soon!</h1>
+			
+		<?php
+   include 'connect.php';
+			$now = (time()/60);
+			$sql = "UPDATE user_table SET
+								available='$now',
+								WHERE username='$username'
+								";
+
+				$result = $mysqli->query($sql);
 
 
-<?php//LOGOUT OF ALL SESSIONS::
-    phpCAS::logout();
-echo "Thanks and have a good day!!";
-
-?>
-
-<body class="citybackground">
-	<h1>Thank you and Have a wonderful Day!!</h1>
-</body>
-
+				if ($mysqli->error) {
+								echo $mysqli->error;
+				}
+			
+				$mysqli->close();
+	
+		?>
+   
+  </body>
 </html>
